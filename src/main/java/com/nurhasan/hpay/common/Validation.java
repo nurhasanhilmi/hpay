8a7340c02b468ef2fcd37244a641940157570ec7
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2023-06-29 21:05:30 +0700

    Add spring general purpose validation class

---
+++ src/main/java/com/nurhasan/hpay/common/Validation.java
@@ -0,0 +1,21 @@
+package com.nurhasan.hpay.common;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+
+import java.util.Set;
+
+import static jakarta.validation.Validation.buildDefaultValidatorFactory;
+
+public class Validation {
+
+    private static final Validator VALIDATOR = buildDefaultValidatorFactory().getValidator();
+
+    public static <T> void validate(T subject) {
+        Set<ConstraintViolation<T>> violations = VALIDATOR.validate(subject);
+
+        if (!violations.isEmpty())
+            throw new ConstraintViolationException(violations);
+    }
+}
